adfbe71dea1346c7a6c24fcffc2b105d0995f6a6
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2025-02-05 10:30:40 +0100

    docs: simplify vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 
-const port = 5173;
-const origin = `${process.env.DDEV_PRIMARY_URL}:${port}`;
-
 // https://vitejs.dev/config/
 export default defineConfig({
 
@@ -23,10 +20,11 @@ export default defineConfig({
   server: {
     // Respond to all network requests
     host: '0.0.0.0',
-    port: port,
+    port: 5173,
     strictPort: true,
-    // Defines the origin of the generated asset URLs during development
-    origin: origin,
+    // Defines the origin of the generated asset URLs during development,
+    // this must be set to the Vite devserver URL and selected port
+    origin: `${process.env.DDEV_PRIMARY_URL}:5173`,
     // Configure CORS securely for the Vite dev server to allow requests
     // from *.ddev.site domains, supports additional hostnames (via regex)
     cors: { origin: /https?:\/\/([A-Za-z0-9\-\.]+)?(\.site)(?::\d+)?$/ },
